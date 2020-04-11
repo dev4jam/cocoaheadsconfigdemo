a100b580a4d3ff718ffcb8ae2fa5ffc5c22a4a09
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2020-04-11 14:29:37 +1000

    Added config model

---
+++ ConfigDemo/Models/Config.swift
@@ -0,0 +1,13 @@
+//
+//  Config.swift
+//  ConfigDemo
+//
+//  Created by Dmitry Klimkin on 11/4/20.
+//  Copyright © 2020 Dmitry Klimkin. All rights reserved.
+//
+
+import Foundation
+
+struct Config {
+    
+}
