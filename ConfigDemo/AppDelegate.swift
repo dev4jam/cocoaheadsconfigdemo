cb0b7620698116df0184714c2b4dd8d5a75d6c4e
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2020-04-11 13:54:48 +1000

    Initial project

---
+++ ConfigDemo/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  ConfigDemo
+//
+//  Created by Dmitry Klimkin on 11/4/20.
+//  Copyright © 2020 Dmitry Klimkin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
